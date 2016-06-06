9134ae8e76e7ee587a2dc5c2477f8989f8834330
jasonwiram <jwiram@gmail.com>
2016-06-06 11:15:13 -0700

    Setup Addition Quiz android studio project

---
+++ app/src/main/java/com/jasonwiram/additionquiz/AdditionQuizActivity.java
@@ -0,0 +1,13 @@
+package com.jasonwiram.additionquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdditionQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addition_quiz);
+    }
+}
